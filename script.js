5f3acaef28de4b5d851d205561de9ec5efc1f379
itwalters22 <66142478+itwalters22@users.noreply.github.com>
2020-07-07 17:59:09 -0400

    Added files for my website!

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function search_blog() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('blogpst'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="list-item";                  
+        } 
+    } 
+} 
+
+
+$.get('top.html',function(response){ 
+    $('#top').html(response); 
+   });
+
+$.get('footer.html',function(response){ 
+    $('#footer').html(response); 
+   });
