2cc8c0ed8417b11e6fa216d0b0be57ea3eac4722
Isa <isabellatwalters@gmail.com>
2020-07-08 19:11:07 -0400

    using this to test my homepage + rss feed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function search_blog() { 
     let input = document.getElementById('searchbar').value 
     input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('blogpst'); 
+    let x = document.getElementsByClassName('blogpost'); 
       
     for (i = 0; i < x.length; i++) {  
         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
